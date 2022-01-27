6e70a295b6cc0f091292805fede2e51b40afe45b
Rachel <rachelmmarriott@gmail.com>
2022-01-26 20:30:11 -0700

    updated html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,39 @@
-// TO DO: when you click a category button, it presents podcast cards for that category in the second column
+var searchFormEl = document.querySelector("#search-form");
+var searchInputEl = document.querySelector("#search");
 var categoryBtn = $("#categories");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    // get value from seach input
+    var search = searchInputEl.value.trim();
+
+    if(search) {
+        getSpotify(search);
+        searchInputEl.value = "";
+    } else {
+        alert("Please enter a podcast category");
+    }
+};
+
+// TO DO: use api fetch to get podcast show info
+var getSpotify = function(show) {
+    var apiUrl = "https://api.spotify.com/v1/search";
+    fetch(apiUrl).then(function(response) {
+        console.log(response);
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+// TO DO: when you click a category button, it presents podcast cards for that category in the second column
+
  
-// TO DO: create array with the fetched api data
+// TO DO: do we need to create array with the fetched api data??
 array = {
-    education: [
-        1, 2, 3
-    ],
-    news: [
-        1, 2, 3
-    ]
+    education: [],
+    news: [],
 };
 
 categoryBtn.on("click", function(event) {
@@ -19,3 +44,4 @@ categoryBtn.on("click", function(event) {
     }
 });
 
+searchFormEl.addEventListener("submit", formSubmitHandler);
